14e0578787244edb9ca193b6c260a53e9578e58d
Sarah Young <y.sarahc@gmail.com>
2014-03-03 17:55:36 +0000

    Added method to define fix

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,24 @@
 class Bike
-	def broken?
+	# the initialize method is always called when you create a new
+	# class by typing Bike.new
+	def intialize
+		# all instance variables begin with "@"
+		# this must be an instance variable because we'll need it
+		# in other methods
+		@broken = false
 	end
+
+	def broken?
+		# instance variables are accessible in all methods
+		@broken
+	end
+
+	def break
+		@broken = true
+	end
+
+	def fix
+		@broken = false
+	end
+
 end
