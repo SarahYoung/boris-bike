7282de0ef77354111fe172c1bbc2887698984175
Sarah Young <y.sarahc@gmail.com>
2014-03-03 17:18:41 +0000

    Adding spec folder to repo

---
+++ spec/bike_spec.rb
@@ -0,0 +1,21 @@
+# link to the Bike class
+require_relative "../lib/bike"
+# we're describing the functionality of a specific class, Bike
+describe Bike do 
+	# this is a specific feature (behaviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do
+		bike = Bike.new # initialise a new object of Bike class
+		# expect an instance of this class to have 
+		# a method "broken?" that should return false
+		expect(bike).not_to be_broken
+		# can also be written: expect(bike.broken?).to be_false
+		# can also be written: bike.broken?.should be(false)
+	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
+end
