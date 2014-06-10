1ca563459106a7b0931b302b0e5a9366f82ceb49
Sarah Young <y.sarahc@gmail.com>
2014-06-10 15:53:05 +0100

    Refactored garage spec

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -10,32 +10,15 @@ describe Garage do
 	let (:garage) { Garage.new }
 	let (:broken_bike) { Bike.new }
 
-	before do
+	it "fixes broken bikes" do
 		broken_bike.break
 		van.dock(broken_bike)
 		van.deliver_broken_bikes_to(garage)
-	end
-	
-	it "collects broken bikes from van" do
-		expect(garage.bikes).to include (broken_bike)
-		expect(van).to be_empty
-	end
-	
-	it "fixes broken bikes" do
 		garage.fix_broken_bikes
 		expect(garage.available_bikes.count).to eq(1)
 	end
-	
-	it "loads working bikes to van" do
-		garage.fix_broken_bikes
-		van.collect_working_bikes_from(garage)
-		expect(garage).to be_empty
-		expect(van.available_bikes.count).to eq(1)
-	end
 
-	context "On initialising" do
-		it "allows setting default capacity" do
-			expect(garage.capacity).to eq(100)
-		end	
-	end
+	it "allows setting default capacity" do
+		expect(garage.capacity).to eq(100)
+	end	
 end
